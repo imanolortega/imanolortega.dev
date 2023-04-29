ac440c16f98bce7747a45c8cbf2a6d2879b22aa3
imanol <imanol@easytechgreen.com>
2023-04-29 02:41:16 -0300

    Clean code

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -15,14 +15,7 @@ export default function ProjectsPage() {
       <div className=" text-slate-800 dark:text-slate-20">
         <Paragraph>{projects()}</Paragraph>
         <div className="grid md:grid-cols-3 grid-cols-1 gap-2 mt-8 mb-2">
-          {projectsList.slice(0, 3).map(({ title, link }) => (
-            <CardLink href={link} key={link}>
-              {title}
-            </CardLink>
-          ))}
-        </div>
-        <div className="grid md:grid-cols-3 grid-cols-1 gap-2 mb-8">
-          {projectsList.slice(3, 6).map(({ title, link }) => (
+          {projectsList.map(({ title, link }) => (
             <CardLink href={link} key={link}>
               {title}
             </CardLink>
